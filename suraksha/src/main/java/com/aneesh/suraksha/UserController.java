4cf97ef5569514382998b4b424a5fc5906d061b8
Aneesh <aneeshpatne@gmail.com>
2025-12-01 10:41:36 +0000

    refactor: Remove unused import in UserController

--- suraksha/src/main/java/com/aneesh/suraksha/UserController.java
+++ suraksha/src/main/java/com/aneesh/suraksha/UserController.java
@@ -3,7 +3,6 @@ package com.aneesh.suraksha;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
