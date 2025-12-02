1bec91310373b69f9ab1c8afd34458140933bb4d
Aneesh <aneeshpatne@gmail.com>
2025-12-02 11:21:10 +0000

    feat: Enhance UserController to handle organisation registration

--- suraksha/src/main/java/com/aneesh/suraksha/users/controller/UserController.java
+++ suraksha/src/main/java/com/aneesh/suraksha/users/controller/UserController.java
@@ -14,6 +14,8 @@ import com.aneesh.suraksha.users.controller.Login.LoginRequest;
 import com.aneesh.suraksha.users.controller.Login.LoginResponse;
 import com.aneesh.suraksha.users.controller.Signup.SignupRequest;
 import com.aneesh.suraksha.users.controller.Signup.SignupResponse;
+import com.aneesh.suraksha.users.model.Organisations;
+import com.aneesh.suraksha.users.model.OrganisationsRepository;
 import com.aneesh.suraksha.users.model.UserEntity;
 import com.aneesh.suraksha.users.model.UserRepository;
 import com.aneesh.suraksha.users.service.HashService;
@@ -23,16 +25,20 @@ import java.util.List;
 @RestController
 public class UserController {
 
+    private final OrganisationsRepository organisationsRepository;
+
     private final LoginService loginService;
 
     private final UserRepository userRepository;
 
     private final HashService hashService;
 
-    public UserController(UserRepository userRepository, HashService hashService, LoginService loginService) {
+    public UserController(UserRepository userRepository, HashService hashService, LoginService loginService,
+            OrganisationsRepository organisationsRepository) {
         this.userRepository = userRepository;
         this.hashService = hashService;
         this.loginService = loginService;
+        this.organisationsRepository = organisationsRepository;
     }
 
     @PostMapping("/signup")
@@ -50,6 +56,11 @@ public class UserController {
     public ResponseEntity<LoginResponse> postMethodName(@Valid @RequestBody LoginRequest entity) {
         LoginResponse res = loginService.login(entity);
         return ResponseEntity.status(res.getStatus() ? HttpStatus.OK : HttpStatus.UNAUTHORIZED).body(res);
+    }
+
+    @PostMapping("/auth/register-organisation")
+    public void postMethodName(@RequestBody Organisations entity) {
+        organisationsRepository.save(entity);
     }
 
 }
