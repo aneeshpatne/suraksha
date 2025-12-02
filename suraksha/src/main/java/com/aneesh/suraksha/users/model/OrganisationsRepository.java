eb52986abd5f76d3e144e277be84fa7056fc7166
Aneesh <aneeshpatne@gmail.com>
2025-12-02 09:00:43 +0000

    feat: Create OrganisationsRepository interface for data access

---
+++ suraksha/src/main/java/com/aneesh/suraksha/users/model/OrganisationsRepository.java
@@ -0,0 +1,7 @@
+package com.aneesh.suraksha.users.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrganisationsRepository extends JpaRepository<Organisations, Long> {
+
+}
