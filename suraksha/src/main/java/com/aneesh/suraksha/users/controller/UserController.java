2e6da240e0e4159217b9d09ede2fff5426195238
Aneesh <aneeshpatne@gmail.com>
2025-12-04 06:43:52 +0000

    feat: Update user onboarding to include JWT token generation and response handling

--- suraksha/src/main/java/com/aneesh/suraksha/users/controller/UserController.java
+++ suraksha/src/main/java/com/aneesh/suraksha/users/controller/UserController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 
 import com.aneesh.suraksha.users.controller.Login.LoginRequest;
@@ -49,8 +50,8 @@ public class UserController {
     }
 
     @PostMapping("/signup")
-    public ResponseEntity<SignupResponse> createUser(@RequestBody SignupRequest entity) {
-        SignupResponse res = hashService.OnBoard(entity);
+    public ResponseEntity<SignupResponse> createUser(@RequestBody SignupRequest entity, HttpServletResponse response) {
+        SignupResponse res = hashService.OnBoard(entity, response);
         return ResponseEntity.status(res.status() ? HttpStatus.OK : HttpStatus.FORBIDDEN).body(res);
     }
 
--- suraksha/src/main/java/com/aneesh/suraksha/users/service/HashService.java
+++ suraksha/src/main/java/com/aneesh/suraksha/users/service/HashService.java
@@ -7,23 +7,29 @@ import com.aneesh.suraksha.users.controller.Signup.SignupRequest;
 import com.aneesh.suraksha.users.controller.Signup.SignupResponse;
 import com.aneesh.suraksha.users.model.UserEntity;
 import com.aneesh.suraksha.users.model.UserRepository;
+
+import jakarta.servlet.http.HttpServletResponse;
+
 import com.aneesh.suraksha.users.model.Organisations;
 import com.aneesh.suraksha.users.model.OrganisationsRepository;
 
 @Service
 public class HashService {
+
+    private final JwtService jwtService;
     private UserRepository userRepository;
     private PasswordEncoder passwordEncoder;
     private OrganisationsRepository organisationsRepository;
 
     public HashService(UserRepository userRepository, PasswordEncoder passwordEncoder,
-            OrganisationsRepository organisationsRepository) {
+            OrganisationsRepository organisationsRepository, JwtService jwtService) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
         this.organisationsRepository = organisationsRepository;
+        this.jwtService = jwtService;
     }
 
-    public SignupResponse OnBoard(SignupRequest entity) {
+    public SignupResponse OnBoard(SignupRequest entity, HttpServletResponse response) {
         Organisations organisation = organisationsRepository.findById(entity.organisationId()).orElse(null);
         if (organisation == null) {
             return new SignupResponse(false, "Organisation Not Found");
@@ -38,6 +44,8 @@ public class HashService {
         String hashedPassword = passwordEncoder.encode(entity.password());
         user.setPassword(hashedPassword);
         userRepository.save(user);
+        String accessToken = jwtService.generateToken(user);
+        String refreshToken = refresh
         return new SignupResponse(true, "User Created Successfully");
     }
 }
