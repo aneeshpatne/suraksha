bfa14f875f1b4c2a91f889b4e9843d6f12bdfcc8
Aneesh <aneeshpatne@gmail.com>
2025-12-10 06:47:43 +0000

    feat: Add Redis connection configuration with LettuceConnectionFactory for Redis integration

---
+++ suraksha/src/main/java/com/aneesh/suraksha/redis/configuration/RedisConnection.java
@@ -0,0 +1,15 @@
+package com.aneesh.suraksha.redis.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+
+@Configuration
+public class RedisConnection {
+    @Bean
+    public RedisConnectionFactory lettuConnectionFactory() {
+        return new LettuceConnectionFactory(new RedisStandaloneConfiguration("server", 6379));
+    }
+}
