5e43c7e8975f3adf7dc15d658bf31f6b3525b887
Aneesh <aneeshpatne@gmail.com>
2025-12-10 07:38:29 +0000

    feat: Implement OTP generation and storage in Redis

--- suraksha/src/main/java/com/aneesh/suraksha/users/service/OtpService.java
+++ suraksha/src/main/java/com/aneesh/suraksha/users/service/OtpService.java
@@ -1,5 +1,8 @@
 package com.aneesh.suraksha.users.service;
 
+import java.security.SecureRandom;
+import java.util.concurrent.TimeUnit;
+
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
@@ -7,8 +10,20 @@ import org.springframework.stereotype.Service;
 public class OtpService {
 
     private final StringRedisTemplate stringRedisTemplate;
+    private final SecureRandom secureRandom = new SecureRandom();
 
     public OtpService(StringRedisTemplate stringRedisTemplate) {
         this.stringRedisTemplate = stringRedisTemplate;
+    }
+
+    private String generateOtp() {
+        int randomNumber = 1000 + secureRandom.nextInt(9000);
+        return Integer.toString(randomNumber);
+    }
+
+    public void OtpFlow() {
+        String otp = generateOtp();
+        String key = "otp:login:" + 1234;
+        stringRedisTemplate.opsForValue().set(key, otp, 5, TimeUnit.MINUTES);
     }
 }
