245777b9ee6805253e51d4abd2393f152a94a9d7
Aneesh <aneeshpatne@gmail.com>
2025-12-30 09:03:59 +0000

    feat: Implement loginWithEmailPassword function for user authentication

---
+++ client/src/auth/authApi.ts
@@ -0,0 +1,19 @@
+export async function loginWithEmailPassword(
+  mailId: string,
+  password: string,
+  organisationId?: string
+) {
+  const res = await fetch("http://localhost:8080/api/v1/auth/token/login", {
+    method: "POST",
+    credentials: "include",
+    headers: {
+      "Content-type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({ mailId, password, organisationId }),
+  });
+  if (!res.ok) {
+    throw new Error(await res.text());
+  }
+  return res.json();
+}
